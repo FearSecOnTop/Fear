c7f8b011f596c32afce231c3fd8857bb702c5e19
FearSecOnTop <90293624+FearSecOnTop@users.noreply.github.com>
2021-09-07 18:36:10 -0700

    Add files via upload

---
+++ Fear.py
@@ -0,0 +1,249 @@
+import os
+import time
+from os import system, name
+from time import sleep
+
+X = str(input("#1 to start me :):"))
+
+if '#1' or '#2' in X:
+	    os.system(' clear ')
+	    time.sleep(0.1)
+	     print(Fore.RED+"                       WHO ARE YOU?                  ")
+       time.sleep(0.1)
+       print(Fore.RED+"                      :>3qKKKKKKKq3>:                ")
+       time.sleep(0.1)
+       print(Fore.RED+"                  ';CpKKKKKKKKKKKKKKKpC;'            ")
+       time.sleep(0.1)
+       print(Fore.RED+"              -iPKKKKKKKKKKKKKKKKKKKKKKKPi-          ")
+       time.sleep(0.1)
+       print(Fore.RED+"          `~v]KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK]v~`    ")
+       time.sleep(0.1)
+       print(Fore.RED+"       ,rwKKKKKKKKKKKKKPv;,:'-':,;vPKKKKKKKKKKKKKwr, ")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKKKKKKKKK/             !KKKKKKKKKKKKKKK!")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKKKKKKKKf               CKKKKKKKKKKKKKK!")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKKKKKKKp-               -qKKKKKKKKKKKKK!")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKKKKKKK>"               "KKKKKKKKKKKKKK!")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKw;,_'-                   .-:,wKKKKKKK! ")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKKhi*                      KKKKKKKKKKKK ")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKKKKKKK;                 ;KKKKKKKKKKKKK!")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKKKKKKK2>'             '>2KKKKKKKKKKKKK!")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKKKKKKKKKZ             ZKKKKKKKKKKKKKKK!")
+       time.sleep(0.1)
+       print(Fore.RED"      !KKKKKKKKKKKKKKK5             eKKKKKKKKKKKKKKK! ")
+       time.sleep(0.1)
+       print(Fore.RED+"      !KKKKKKKKKKKqC;-               -;CqKKKKKKKKKKK!")
+       time.sleep(0.1)
+       print(Fore.RED+"      <KKKKKKKKkr,                       ,rSKKKKKKKK<")
+       time.sleep(0.1)
+       print(Fore.RED+"       -v]qj;-                             -;jq]v-   ")
+       time.sleep(1.0)
+       os.system(' clear ')
+      print(Fore.MAGENTA+"            Coded By @fearsecurity                         ")
+      time.sleep(0.1)
+      print(Fore.MAGENTA+"███████╗███████╗ █████╗ ██████╗ ███████╗███████╗ ██████╗   ")
+      time.sleep(0.1)
+      print(Fore.MAGENTA+"██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝   ")
+      time.sleep(0.1)
+      print(Fore.MAGENTA+"█████╗  █████╗  ███████║██████╔╝███████╗█████╗  ██║        ")
+      time.sleep(0.1)
+      print(Fore.MAGENTA+"██╔══╝  ██╔══╝  ██╔══██║██╔══██╗╚════██║██╔══╝  ██║        ")
+      time.sleep(0.1)
+      print(Fore.MAGENTA+"██║     ███████╗██║  ██║██║  ██║███████║███████╗╚██████╗   ")
+      time.sleep(0.1)
+      print(Fore.MAGENTA+"╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝   ")
+      time.sleep(0.1)
+      print(Fore.RED+"                 Coded By @fearsecurity                        ")
+      time.sleep(1.0)                                                   
+      print(Fore.RED+"...............................................................")
+      print(Fore.BLUE+"███████╗███████╗ █████╗ ██████╗ ███████╗███████╗ ██████╗      ")
+      time.sleep(0.1)
+      print(Fore.RED+" ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝      ")
+      time.sleep(0.1)
+      print(Fore.BLUE+"█████╗  █████╗  ███████║██████╔╝███████╗█████╗  ██║           ")
+      time.sleep(0.1)
+      print(Fore.RED+" ██╔══╝  ██╔══╝  ██╔══██║██╔══██╗╚════██║██╔══╝  ██║           ")
+      time.sleep(0.1)
+      print(Fore.BLUE+"██║     ███████╗██║  ██║██║  ██║███████║███████╗╚██████╗      ")
+      time.sleep(0.1)
+      print(Fore.RED+" ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝      ")
+      time.sleep(0.1)
+      print(Fore.BLUE+"..............................................................")
+      time.sleep(0.1)
+      print(Fore.RED+":.........................HTTP DDoS............................")
+      time.sleep(0.1)
+      print(Fore.BLUE+":option A: IHulk                                              :")
+      time.sleep(0.1)
+      print(Fore.RED+" :option B: Xerxes                                             :")
+      time.sleep(0.1)
+      print(Fore.BLUE+":option C: MHDDoS                                             :")
+      time.sleep(0.1)
+      print(Fore.RED+" :option D: DDoS-Ripper                                        :")
+      time.sleep(0.1)
+      print(Fore.BLUE+".......................misc options..........................:")
+      time.sleep(0.1)
+      print(Fore.RED+" :option Y: install all requirements ::: this is required      :")
+      time.sleep(0.1)
+      print(Fore.BLUE+":option Z: Socials                                            :")
+      time.sleep(0.1)
+      print(Fore.RED+" :............................................................:")
+      time.sleep(0.1)
+      print(Fore.BLUE+"..........................IP Tracker.........................:")
+      time.sleep(0.1)
+      print(Fore.RED+" :option E: IP-Tracer:                                        :")
+      time.sleep(0.1)
+      print(Fore.BLUE+":option F track-ip:                                          :")
+      time.sleep(0.1)
+      print(Fore.RED+" :option G N-TRACE:                                           :")
+      time.sleep(0.1)
+      print(Fore.BLUE+":option H SkidTracer:                                        :")
+      time.sleep(0.1)
+      print(Fore.RED+" :..............................OSINT.........................:")
+      time.sleep(0.1)
+      print(Fore.BLUE+":option I: sherlock:                                         :")
+      time.sleep(0.1)
+      print(Fore.RED+" :option J: twint:                                            :")
+      time.sleep(0.1)
+      print(Fore.BLUE+":option K: GHunt:                                            :")
+      time.sleep(0.1)
+      print(Fore.RED+" :option L: spiderfoot:                                       :")
+      time.sleep(0.1)
+      print(Fore.BLUE+":option M: TheHarvester:                                     :")
+      time.sleep(0.1)
+      print(Fore.RED+" :............................................................:")
+      time.sleep(0.1)
+      print(Fore.BLUE+"...............................Credits.......................:")
+      time.sleep(0.1)
+      print(Fore.RED+" @fearsecurity                                                :")
+      time.sleep(0.1)
+      
+      S = str(input("OPTION:"))
+
+      if 'Z' in S:
+        time.sleep(2)
+        print(Fore.BLUE+"..........................................................")
+        print(Fore.BLUE+"███████╗███████╗ █████╗ ██████╗ ███████╗███████╗ ██████╗  ")
+        print(Fore.BLUE+"██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝  ")
+        print(Fore.BLUE+"█████╗  █████╗  ███████║██████╔╝███████╗█████╗  ██║       ")
+        print(Fore.BLUE+"██╔══╝  ██╔══╝  ██╔══██║██╔══██╗╚════██║██╔══╝  ██║       ")
+        print(Fore.BLUE+"██║     ███████╗██║  ██║██║  ██║███████║███████╗╚██████╗  ")
+        print(Fore.BLUE+"╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝  ")
+        print(Fore.BLUE+"..........................................................")
+        print(Fore.RED+" instagram: fearsecurity                                   ")
+        print(Fore.BLUE+"..........................................................")
+        print(Fore.RED+" ..........................................................")
+        print(Fore.BLUE+".......................NOTICE.............................")
+        print(Fore.RED+" : I AM NOT RESPONISBLE FOR ANY MISUSE OF THESE TOOLS...   ")
+        print(Fore.BLUE+": OR THIS FRAMEWORK, WE HIGHLY ADVISE YOU DO NOT USE THIS ")
+        print(Fore.RED+" : USE THIS FOR ILLEGAL ACTIONS...                         ")
+        print(Fore.BLUE+"..........................................................")
+        
+        if 'A' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        O = str(input(" whats the host youngling: "))
+        os.system(' cd ihulk && python3 ihulk.py {O} ')
+
+        if 'B' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' cd XERXES && gcc -o xerxes xerxes.c ')   
+
+        if 'C' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' cd MHDDoS && pip3 install -r requirements.txt && python3 start.py ')
+
+        if 'D' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' cd DDoS-Ripper && python3 DRipper.py ')
+        V = str(input(" Whats the IP youngling: "))
+        B = str(input(" P O R T : "))
+        os.system(f' python3 DRipper.py -s {V} -p {B} ')
+
+        if 'Y' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' sudo apt install nmap php -y git clone https://github.com/twintproject/twint && git clone https://github.com/laramies/theHarvester && git clone https://github.com/iamaamir/ihulk.py && git clone https://github.com/MHProDev/MHDDoS && git clone https://github.com/XCHADXFAQ77X/XERXES && git clone https://github.com/smicallef/spiderfoot && git clone https://github.com/mxrch/GHunt && git clone https://github.com/sherlock-project/sherlock && git clone https://github.com/rajkumardusad/IP-Tracer && git clone https://github.com/htr-tech/track-ip && git clone https://github.com/Nabil-Official/N-TRACE && git clone https://github.com/Z3NTL3/SkidTracer ') 
+
+        if 'E' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' cd IP-Tracer && chmod +x install && ./install ')
+
+        if 'F' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' cd track-ip && bash trackip ')   
+
+        if 'G' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' cd N-TRACE && chmod +x setup.sh && ./setup.sh ')
+
+       if 'H' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' cd SkidTracer && chmod+x skid-tracer-m && ./skid-tracer-m ')
+
+      if 'I' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' cd sherlock && pip3 install -r requirements.txt ')
+        A = str(input(" Username: "))
+        os.system(' python3 sherlock.py {A} ')
+      if 'J' in S:
+        time.sleep(2)
+        os.system(' clear ')
+        os.system(' cd twint && pip3 install -r requirements.txt && python3 setup.py ')
+        print(Fore.RED+" Commands... ")
+        print """ twint -u username - Scrape all the Tweets of a user (doesn't include retweets but includes replies).
+twint -u username -s pineapple - Scrape all Tweets from the user's timeline containing pineapple.
+twint -s pineapple - Collect every Tweet containing pineapple from everyone's Tweets.
+twint -u username --year 2014 - Collect Tweets that were tweeted before 2014.
+twint -u username --since "2015-12-20 20:30:15" - Collect Tweets that were tweeted since 2015-12-20 20:30:15.
+twint -u username --since 2015-12-20 - Collect Tweets that were tweeted since 2015-12-20 00:00:00.
+twint -u username -o file.txt - Scrape Tweets and save to file.txt.
+twint -u username -o file.csv --csv - Scrape Tweets and save as a csv file.
+twint -u username --email --phone - Show Tweets that might have phone numbers or email addresses.
+twint -s "Donald Trump" --verified - Display Tweets by verified users that Tweeted about Donald Trump.
+twint -g="48.880048,2.385939,1km" -o file.csv --csv - Scrape Tweets from a radius of 1km around a place in Paris and export them to a csv file.
+twint -u username -es localhost:9200 - Output Tweets to Elasticsearch
+twint -u username -o file.json --json - Scrape Tweets and save as a json file.
+twint -u username --database tweets.db - Save Tweets to a SQLite database.
+twint -u username --followers - Scrape a Twitter user's followers.
+twint -u username --following - Scrape who a Twitter user follows.
+twint -u username --favorites - Collect all the Tweets a user has favorited (gathers ~3200 tweet).
+twint -u username --following --user-full - Collect full user information a person follows
+twint -u username --timeline - Use an effective method to gather Tweets from a user's profile (Gathers ~3200 Tweets, including retweets & replies).
+twint -u username --retweets - Use a quick method to gather the last 900 Tweets (that includes retweets) from a user's profile.
+twint -u username --resume resume_file.txt - Resume a search starting from the last saved scroll-id. """
+
+if 'K' in S:
+  time.sleep(2)
+  os.system(' clear ')
+  os.system(' cd GHunt && pip3 install -r requirements.txt && python3 check_and_gen.py ')
+  C = str(input(" Email: "))
+  os.system(' python3 ghunt email {C} ')
+
+if 'L' in S:
+  time.sleep(2)
+  os.system(' clear ')
+  os.system(' cd spiderfoot && pip3 install -r requirements.txt ')
+  D = str(input(" IP: "))
+  E = str(input(" PORT: "))
+  os.system(' python3 ./sf.py -l {D}:{E} ')
+
+if 'M' in S:
+  time.sleep(2)
+  os.system(' clear ')
+  os.system(' cd theHarvester && pip3 install -r requirements.txt && python3 theHarvester.py ')
