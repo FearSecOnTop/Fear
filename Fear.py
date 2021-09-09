a1fccd90e533076d9f9d1fa198284310f595d181
Auschwitz <90293624+FearSecOnTop@users.noreply.github.com>
2021-09-08 20:23:55 -0700

    Update Fear.py

--- Fear.py
+++ Fear.py
@@ -6,8 +6,7 @@ from time import sleep
 X = str(input("#1 to start me :):"))
 
 if '#1' or '#2' in X:
-     os.system(' clear ')
-       print(Fore.RED+"                       WHO ARE YOU?                  ")
+       os.system(' clear ')
        time.sleep(0.1)
        print(Fore.RED+"                      :>3qKKKKKKKq3>:                ")
        time.sleep(0.1)
